073d24cdde342f4523b6244a644a0ed3b2d2829f
Allain Lalonde <allain.lalonde@gmail.com>
2015-01-08 00:11:13 -0500

    support strictness specification
    doubles the length but ensures the conditional only gets executed once.

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var prop = require("prop");
-module.exports = function isProp(property,value){
-	return function(object){
-		return prop(property)(object) == value;
+module.exports = function isProp(property, value, strict){
+	if (strict === true) {
+		return function(object){
+			return prop(property)(object) === value;
+		}
+	} else {
+		return function(object){
+			return prop(property)(object) == value;
+		}
 	}
 }
