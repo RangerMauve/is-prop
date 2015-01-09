db07adf3bf86c4add20a92d0b525285b1f1c753e
RangerMauve <RangerMauve@htomail.com>
2015-01-08 22:21:11 -0500

    Ran beautifier

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var prop = require("prop");
-module.exports = function isProp(property, value, strict){
+module.exports = function isProp(property, value, strict) {
 	if (strict === true) {
-		return function(object){
+		return function(object) {
 			return prop(property)(object) === value;
 		}
 	} else {
-		return function(object){
+		return function(object) {
 			return prop(property)(object) == value;
 		}
 	}
